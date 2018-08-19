34efaf49d67e8ef215411a8c287f297c3f277cb7
Lasath Fernando <devel@lasath.org>
2018-08-18 21:03:54 -0700

    Read volume from request

--- main.ts
+++ main.ts
@@ -1,13 +1,22 @@
 import * as express from 'express';
 
 const PORT = 55381;
+const DEFAULT_VOLUME = 30;
 
 const app = express();
 
 app.get("/avr/volume", (req, res) => {
-    console.log("Doing a thing");
+    if (req.query['key'] !== 'Key1') {
+        res.status(400).send('BAD REQUEST\n');
+        return;
+    }
 
-    res.status(200).send("OK");
+    // console.log(JSON.stringify(req.query, null, 4));
+    console.log(`Setting avr volume to ${req.query['number'] || DEFAULT_VOLUME}`);
+
+    
+
+    res.status(200).send("OK\n");
 });
 
 app.listen(PORT, () => console.log(`Initialized. Listening on ${PORT}`));
