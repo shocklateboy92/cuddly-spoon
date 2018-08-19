9a32a69e4abcbebc41c3b5749d44ced5ae6287d4
Lasath Fernando <devel@lasath.org>
2018-08-18 21:43:34 -0700

    Implement logic to talk to AVR

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import * as express from 'express';
+import * as express from "express";
+import * as net from "net";
 
 const PORT = 55381;
 const DEFAULT_VOLUME = 30;
@@ -6,17 +7,46 @@ const DEFAULT_VOLUME = 30;
 const app = express();
 
 app.get("/avr/volume", (req, res) => {
-    if (req.query['key'] !== 'Key1') {
-        res.status(400).send('BAD REQUEST\n');
+    if (req.query["key"] !== "Key1") {
+        res.status(400).send("BAD REQUEST\n");
         return;
     }
 
     // console.log(JSON.stringify(req.query, null, 4));
-    console.log(`Setting avr volume to ${req.query['number'] || DEFAULT_VOLUME}`);
 
-    
+    let targetVol = req.query["number"] || DEFAULT_VOLUME;
+    if (targetVol < 10) {
+        targetVol *= 10;
+    }
+    console.log(`Setting avr volume to ${targetVol}`);
 
-    res.status(200).send("OK\n");
+    const client = new net.Socket();
+
+    client.setTimeout(2000);
+    client.on('timeout', () => {
+        client.end();
+        res.status(500).send('TELENT SOCKET TIMEOUT');
+    })
+
+    client.on("data", data => {
+        console.log('Response: ' + data.toString());
+        client.destroy();
+
+        res.status(200).send("OK\n");
+    });
+
+    client.connect(
+        23,
+        "Denon-AVR-S940H.home.lasath.org",
+        () => {
+            const msg = `MV${targetVol}`;
+            console.log(`Connected to AVR. Sending '${msg}'...`);
+
+            client.write(msg, () => {
+                console.log('Data written');
+            });
+        }
+    );
 });
 
 app.listen(PORT, () => console.log(`Initialized. Listening on ${PORT}`));
